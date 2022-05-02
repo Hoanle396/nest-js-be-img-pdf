9f6920e29456a8707eba119b3ea5553b73959a52
Hoanle396 <89791546+Hoanle396@users.noreply.github.com>
2022-05-02 23:28:06 +0700

    i

--- dist/user/user.module.js
+++ dist/user/user.module.js
@@ -12,13 +12,22 @@ const user_service_1 = require("./user.service");
 const user_controller_1 = require("./user.controller");
 const typeorm_1 = require("@nestjs/typeorm");
 const user_entity_1 = require("./entities/user.entity");
+const jwt_1 = require("@nestjs/jwt");
+const constants_1 = require("./constants");
+const passport_1 = require("@nestjs/passport");
 let UserModule = class UserModule {
 };
 UserModule = __decorate([
     (0, common_1.Module)({
         imports: [typeorm_1.TypeOrmModule.forFeature([
                 user_entity_1.User
-            ])],
+            ]),
+            passport_1.PassportModule,
+            jwt_1.JwtModule.register({
+                secret: constants_1.jwtConstants.secret,
+                signOptions: { expiresIn: '3600s' },
+            }),
+        ],
         controllers: [user_controller_1.UserController],
         providers: [user_service_1.UserService]
     })
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,20 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [TypeOrmModule.forFeature([
     User
-  ])],
+  ]),
+  PassportModule,
+  JwtModule.register({
+    secret: jwtConstants.secret,
+    signOptions: { expiresIn: '3600s' },
+  }),
+  ],
   controllers: [UserController],
   providers: [UserService]
 })
